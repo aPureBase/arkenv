c2b4cc7135a2fd23e0ced21cd4d2dd08a718acf0
Andreas Volkmann <avo@apurebase.com>
2018-06-25 12:34:53 +0200

    add publishing config for release 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,12 +9,14 @@ import org.gradle.kotlin.dsl.getValue
 import org.gradle.kotlin.dsl.kotlin
 import org.gradle.kotlin.dsl.repositories
 import org.jetbrains.kotlin.gradle.dsl.Coroutines
+import org.gradle.api.tasks.bundling.Jar
 
 group = "com.apurebase"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 plugins {
     kotlin("jvm") version "1.2.50"
+    `maven-publish`
 }
 
 val junitVersion = "5.2.0"
@@ -34,4 +36,31 @@ dependencies {
     testCompile("org.junit.jupiter", "junit-jupiter-api", junitVersion)
     testCompile("org.amshove.kluent", "kluent", "1.38")
     testCompile("org.jmockit:jmockit:1.39")
+    testRuntime("org.junit.jupiter", "junit-jupiter-engine", junitVersion)
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+    classifier = "sources"
+    from(java.sourceSets["main"].allSource)
+}
+
+
+val privateMavenWriteUrl: String by project
+val privateMavenPassword: String by project
+publishing {
+    repositories {
+        maven {
+            url = uri(privateMavenWriteUrl)
+            credentials {
+                username = "myMavenRepo"
+                password = privateMavenPassword
+            }
+        }
+    }
+    (publications) {
+        "mavenJava"(MavenPublication::class) {
+            from(components["java"])
+            artifact(sourcesJar)
+        }
+    }
 }
