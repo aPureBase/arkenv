6a9b02f3aa600a4c345d0b0205094a1ebf14d334
AndreasVolkmann <avo@apurebase.com>
2019-01-18 15:21:00 +0100

    refactor EnvParser

--- src/main/kotlin/com/apurebase/arkenv/EnvParser.kt
+++ src/main/kotlin/com/apurebase/arkenv/EnvParser.kt
@@ -5,19 +5,20 @@ import java.io.File
 internal fun getEnvValue(argument: Argument<*>, enableEnvSecrets: Boolean): String? {
     // If an envVariable is defined we'll pick this as highest order value
     argument.envVariable?.let {
-        val definedEnvValue = System.getenv(it) ?: getEnvSecret(it, enableEnvSecrets)
+        val definedEnvValue = getEnv(it, enableEnvSecrets)
         if (!definedEnvValue.isNullOrEmpty()) return definedEnvValue
     }
 
     // Loop over all argument names and pick the first one that matches
     return argument.names
         .filter(String::isAdvancedName)
-        .mapNotNull {
-            val lookup = argument.envPrefix + it.toSnakeCase()
-            System.getenv(lookup) ?: getEnvSecret(lookup, enableEnvSecrets)
-        }
+        .map { argument.envPrefix + it.toSnakeCase() }
+        .mapNotNull { getEnv(it, enableEnvSecrets) }
         .firstOrNull()
 }
+
+private fun getEnv(name: String, enableEnvSecrets: Boolean) =
+    System.getenv(name) ?: getEnvSecret(name, enableEnvSecrets)
 
 private fun getEnvSecret(lookup: String, enableEnvSecrets: Boolean): String? = when {
     enableEnvSecrets -> System.getenv("${lookup}_FILE")?.let(::File)?.readText()
