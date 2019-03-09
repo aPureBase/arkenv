1036e02adb49f8638ab3f7bfcff9bd7d7075d0ce
AndreasVolkmann <avo@apurebase.com>
2019-03-09 14:18:44 +0100

    add more tests for profiles

--- src/test/kotlin/com/apurebase/arkenv/feature/ProfileFeatureTest.kt
+++ src/test/kotlin/com/apurebase/arkenv/feature/ProfileFeatureTest.kt
@@ -2,37 +2,64 @@ package com.apurebase.arkenv.feature
 
 import com.apurebase.arkenv.*
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import strikt.api.Assertion
 import strikt.assertions.isEqualTo
 
 internal class ProfileFeatureTest {
 
-    private class Ark : Arkenv() {
+    private class Ark(vararg arguments: String) : Arkenv() {
         init {
             install(ProfileFeature())
+            parse(arguments.toList().toTypedArray())
         }
 
         val port: Int by argument("--port")
         val name: String by argument("--name")
+
+        val other: String? by argument("-o", "--other")
     }
 
     @Test fun `default profile should parse`() {
-        val args = arrayOf<String>()
         Ark()
-            .parse(args)
-            .expectThat {
-                get { port }.isEqualTo(80)
-                get { name }.isEqualTo("profile-test")
-            }
+            .expectThat { isDefault() }
     }
 
     @Test fun `nested profile should parse correctly`() {
-        val args = arrayOf("--profile", "dev")
-        Ark()
-            .parse(args)
-            .expectThat {
-                get { port }.isEqualTo(5000)
-                get { name }.isEqualTo("profile-test")
-            }
+        Ark("--profile", "dev")
+            .expectThat { isDevelopment() }
     }
 
+    @Test fun `production profile`() {
+        Ark("--profile", "prod")
+            .expectThat { isProduction() }
+    }
+
+    @Test fun `combine with other sources`() {
+        Ark("-o", "test", "profile=dev")
+            .expectThat { isDevelopment("test") }
+    }
+
+    @Test fun `should throw when profile cannot be found`() {
+        assertThrows<NullPointerException> {
+            Ark("--profile", "wrong")
+        }
+    }
+
+    @Test fun `set profile via env`() {
+        MockSystem("PROFILE" to "prod")
+        Ark().expectThat { isProduction() }
+    }
+
+    private fun Assertion.Builder<Ark>.isDefault() = expect(80, "profile-test", null)
+
+    private fun Assertion.Builder<Ark>.isDevelopment(other: String? = null) = expect(5000, "profile-test", other)
+
+    private fun Assertion.Builder<Ark>.isProduction() = expect(443, "production", null)
+
+    private fun Assertion.Builder<Ark>.expect(port: Int, name: String, other: String? = null) {
+        get { this.port }.isEqualTo(port)
+        get { this.name }.isEqualTo(name)
+        get { this.other }.isEqualTo(other)
+    }
 }
