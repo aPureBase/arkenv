b1ced5f6f2f8a806f8958bbe1830c43c1e5c0b1d
AndreasVolkmann <avo@apurebase.com>
2019-03-13 19:46:50 +0100

    more tests of propert feature

--- src/test/kotlin/com/apurebase/arkenv/feature/PropertyFeatureTests.kt
+++ src/test/kotlin/com/apurebase/arkenv/feature/PropertyFeatureTests.kt
@@ -1,16 +1,16 @@
 package com.apurebase.arkenv.feature
 
 import com.apurebase.arkenv.*
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.*
 import strikt.assertions.isEqualTo
 import java.io.File
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class PropertyFeatureTests {
 
-    private class PropertiesArk(propertiesFile: String) : Arkenv() {
+    private class PropertiesArk(propertiesFile: String, locations: List<String>) : Arkenv() {
         init {
-            install(PropertyFeature(propertiesFile))
+            install(PropertyFeature(propertiesFile, locations))
         }
 
         val mysqlPassword: String by argument("--mysql-password")
@@ -26,41 +26,63 @@ class PropertyFeatureTests {
         verify("app_lower.properties")
     }
 
-    private fun verify(path: String) {
-        PropertiesArk(path).parse(arrayOf()).expectThat {
-            get { mysqlPassword }.isEqualTo("this_is_expected")
-            get { port }.isEqualTo(5050)
-            get { multiLine }.isEqualTo("this stretches lines")
+    private fun verify(
+        path: String,
+        port: Int = 5050,
+        pw: String = "this_is_expected",
+        locations: List<String> = listOf("", "config/")
+    ) {
+        PropertiesArk(path, locations).parse(arrayOf()).expectThat {
+            get { this.mysqlPassword }.isEqualTo(pw)
+            get { this.port }.isEqualTo(port)
+            get { this.multiLine }.isEqualTo("this stretches lines")
         }
     }
 
     @Test fun `should throw when dot env file can not be found`() {
-        val ark = PropertiesArk("does_not_exist.env")
+        val ark = PropertiesArk("does_not_exist.env", listOf())
         assertThrows<NullPointerException> {
             ark.parse(arrayOf())
         }
     }
 
-    @Test fun `should be able to load from file`() {
-        val name = "file_based_props.properties"
-        val content = this::class.java.classLoader.getResource("app.properties").readText()
-        File(name).let {
-            it.deleteOnExit()
-            it.writeText(content)
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    inner class Customization {
+        private val content = this::class.java.classLoader.getResource("app.properties").readText()
+        private val name = "file_based_props.properties"
+        private val dir = File("config")
+
+        @Test fun `should be able to load from file`() {
+            File(name).let {
+                it.deleteOnExit()
+                it.writeText(content)
+            }
+            verify(name)
         }
-        verify(name)
+
+        @Test fun `default file config`() {
+            dir.mkdirs()
+            File(dir, name).let {
+                it.deleteOnExit()
+                it.writeText(content)
+            }
+            verify(name)
+            dir.deleteRecursively()
+        }
+
+        @Test fun `default classpath config`() {
+            verify("nested.properties", 1212, "classpath config")
+        }
+
+        @Test fun `custom classpath config`() {
+            verify("cp.properties", 4545, "custom nested classpath config", locations = listOf("custom/path/"))
+        }
+
+        @AfterEach fun afterEach() {
+            File(name).delete()
+            dir.deleteRecursively()
+        }
     }
 
-    @Test fun `should load from default config`() {
-        val name = "file_based_props.properties"
-        val content = this::class.java.classLoader.getResource("app.properties").readText()
-        val root = File("config")
-        root.mkdirs()
-        File(root, name).let {
-            it.deleteOnExit()
-            it.writeText(content)
-        }
-        verify(name)
-        root.deleteRecursively()
-    }
 }
