ca2d77515e8ebc32e1f8e24f0ab8df1cc173b3d7
AndreasVolkmann <avo@apurebase.com>
2019-06-08 12:22:48 +0200

    Added decryption support to HttpFeature

--- src/main/kotlin/com/apurebase/arkenv/feature/HttpFeature.kt
+++ src/main/kotlin/com/apurebase/arkenv/feature/HttpFeature.kt
@@ -1,38 +1,56 @@
 package com.apurebase.arkenv.feature
 
 import com.apurebase.arkenv.Arkenv
-import com.apurebase.arkenv.ensureEndsWith
 import java.io.InputStream
 import java.net.URL
+import javax.crypto.Cipher
+import javax.xml.bind.DatatypeConverter
 
 class HttpFeature(
     private val rootUrl: String,
-    private val application: String? = null,
-    private val httpClient: HttpClient = HttpClientImpl()
+    private val applicationName: String? = null,
+    private val profile: String? = null,
+    private val label: String? = null,
+    private val httpClient: HttpClient = HttpClientImpl(),
+    private val cipher: Cipher? = null,
+    private val keyword: String = "{cipher}"
 ) : ArkenvFeature {
+
+    private val suffix = "properties"
 
     override fun onLoad(arkenv: Arkenv) {
         httpClient
             .get(makeUrl(arkenv.programName))
             .use(PropertyFeature.Companion::parseProperties)
+            .let(::decryptData)
             .let(arkenv.keyValue::putAll)
     }
 
-    private fun makeUrl(programName: String): String {
-        val suffix = "properties"
-        return "${rootUrl.ensureEndsWith('/')}${application ?: programName}.$suffix".also(::println)
-    }
+    private fun decryptData(data: Map<String, String>): Map<String, String> =
+        cipher?.let {
+            data.mapValues { (_, value) ->
+                if (value.startsWith(keyword)) it.decrypt(value)
+                else value
+            }
+        } ?: data
 
-    fun encryption() {
-        TODO()
+    private fun Cipher.decrypt(input: String): String =
+        input.removePrefix(keyword)
+            .let { DatatypeConverter.parseHexBinary(it) }
+            .let(::doFinal)
+            .let { String(it) }
+
+    private fun makeUrl(programName: String): String {
+        val root = rootUrl.removeSuffix("/")
+        val name = applicationName ?: programName
+        return "$root/$name.$suffix"
     }
 }
 
 interface HttpClient {
-
-    fun get(url: String): InputStream = URL(url).openStream()
-
+    fun get(url: String): InputStream
 }
 
-class HttpClientImpl : HttpClient
-
+class HttpClientImpl : HttpClient {
+    override fun get(url: String): InputStream = URL(url).openStream()
+}
--- src/test/kotlin/com/apurebase/arkenv/HttpFeatureTest.kt
+++ src/test/kotlin/com/apurebase/arkenv/HttpFeatureTest.kt
@@ -7,17 +7,21 @@ import com.apurebase.arkenv.test.parse
 import org.junit.jupiter.api.Test
 import strikt.assertions.isEqualTo
 import java.io.InputStream
+import java.security.Key
+import java.security.KeyPair
+import java.security.KeyPairGenerator
+import javax.crypto.Cipher
+import javax.xml.bind.DatatypeConverter
 
 class HttpFeatureTest {
 
-    private class Ark(url: String) : Arkenv("Arkenv-Client", {
-        install(HttpFeature(url, null, MockClient()))
+    private inner class Ark(url: String) : Arkenv("Arkenv-Client", {
+        install(HttpFeature(url, httpClient = MockClient(), cipher = decryptCipher))
     }) {
         val message: String by argument()
-
         val status: Int by argument()
-
         val nested: Int by argument("nested.item")
+        val password: String by argument("spring.datasource.password")
     }
 
     @Test fun test() {
@@ -27,14 +31,32 @@ class HttpFeatureTest {
                 get { message }.isEqualTo("Hello world")
                 get { status }.isEqualTo(100)
                 get { nested }.isEqualTo(1)
+                get { password }.isEqualTo("mysecret")
             }
     }
 
-    private class MockClient : HttpClient {
+    private fun generateKey(): KeyPair =
+        KeyPairGenerator
+            .getInstance("RSA")
+            .apply { initialize(512) }
+            .genKeyPair()
+
+    private val keyPair = generateKey()
+    private val encryptCipher = keyPair.public.makeCipher(Cipher.ENCRYPT_MODE)
+    private val decryptCipher = keyPair.private.makeCipher(Cipher.DECRYPT_MODE)
+    private val password = "mysecret"
+    private val encryptedPassword = encryptCipher
+        .doFinal(password.toByteArray())
+        .let { DatatypeConverter.printHexBinary(it) }
+
+    private fun Key.makeCipher(mode: Int): Cipher = Cipher.getInstance("RSA").also { it.init(mode, this) }
+
+    private inner class MockClient : HttpClient {
         private val response = """
                 message: Hello world
                 nested.item: 1
                 status: 100
+                spring.datasource.password: {cipher}$encryptedPassword
             """.trimIndent()
 
         override fun get(url: String): InputStream = response.byteInputStream()
