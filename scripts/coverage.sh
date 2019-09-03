c20661ca44801f42c3174e814c3dabbcd3031028
AndreasVolkmann <avo@apurebase.com>
2019-09-03 20:27:11 +0200

    Fix jacoco root report path

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-path="${1:-build/reports/jacocoRootReport/jacocoRootReport.csv}"
+path="${1:-build/reports/jacoco/jacocoRootReport/jacocoRootReport.csv}"
 
 awk -F "," '
     { instructions += $4 + $5; covered += $5 }
