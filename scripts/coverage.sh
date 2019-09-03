33c1d3dc117a44a62d8e8f373c1fd054db18d1a7
AndreasVolkmann <avo@apurebase.com>
2019-09-03 20:16:06 +0200

    Add gradle jacocoRootReport task for coverage

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,28 +1,10 @@
 #!/usr/bin/env sh
 
-coreDir=arkenv
-yamlDir=arkenv-yaml
-path="${1:-build/reports/test/jacocoTestReport.csv}"
+path="${1:-build/reports/jacocoRootReport/jacocoRootReport.csv}"
 
-getStats() {
-  awk -F "," '
+awk -F "," '
     { instructions += $4 + $5; covered += $5 }
     END {
-        print covered, instructions
+        print 100 * covered / instructions, "% covered"
      }
-   ' "$1/$2"
-}
-
-res="$(getStats "$coreDir" "$path")"
-covered=$(echo "$res" | awk '{print $1}')
-instructions=$(echo "$res" | awk '{print $2}')
-
-res="$(getStats "$yamlDir" "$path")"
-covered2=$(echo "$res" | awk '{print $1}')
-instructions2=$(echo "$res" | awk '{print $2}')
-
-cov=$((covered + covered2))
-ins=$((instructions + instructions2))
-
-result=$((100 * cov / ins))
-echo $result % covered
+   ' "${path}"
