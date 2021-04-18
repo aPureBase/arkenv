b4c830b4cda9351c0f8ef0fc51296b6e21f2968a
Andreas Volkmann <anvolkma@microsoft.com>
2021-04-18 15:33:21 +0200

    Refactor cli argument parsing, support single quote assignments

--- arkenv/src/main/kotlin/com/apurebase/arkenv/util/StringUtils.kt
+++ arkenv/src/main/kotlin/com/apurebase/arkenv/util/StringUtils.kt
@@ -6,19 +6,12 @@ internal fun String.toSnakeCase() = this
     .toUpperCase()
     .removePrefixes("_")
 
-internal fun String.removePrefixes(prefix: CharSequence): String = this
+private fun String.removePrefixes(prefix: CharSequence): String = this
     .removePrefix(prefix)
     .let {
         if (it.startsWith(prefix)) it.removePrefixes(prefix)
         else it
     }
-
-internal fun String.endsWith(list: Iterable<String>): Boolean = list.any(::endsWith)
-
-internal fun String.startsWith(list: Iterable<String>): Boolean = list.any(::startsWith)
-
-internal fun String.removeSurrounding(list: Iterable<String>): String =
-    list.fold(this) { acc, s -> acc.removeSurrounding(s) }
 
 internal fun String.isAdvancedName() = startsWith("--")
 
