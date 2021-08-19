16f014cd6c624394a505c2b39ea1e051ae58d105
Andreas Volkmann <anvolkma@microsoft.com>
2021-08-19 13:34:55 +0200

    Up versions

--- arkenv/build.gradle.kts
+++ arkenv/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     base
-    kotlin("jvm") version "1.4.32"
-    id("org.jetbrains.dokka") version "1.4.32"
+    kotlin("jvm") version "1.5.10"
+    id("org.jetbrains.dokka") version "1.5.0"
     id("java-test-fixtures")
     signing
 }
